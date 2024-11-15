47bf79c4acec9a17d29d97c3d1171bdab3b1b136
lenovo <lenovo@DESKTOP-9FL00AT>
2024-11-15 13:33:27 +0530

    After modification

---
+++ Selenium/src/gitHub/GitHub.java
@@ -0,0 +1,12 @@
+package gitHub;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GitHub {
+@Test
+public void addMessage()
+{
+	Reporter.log("After modification we have to commit and the push" ,true);
+}
+}
