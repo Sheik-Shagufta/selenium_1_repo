19d3d0e39b838c8b5756931d8f655a9b4a1beb09
Sheik-Shagufta <shaguftasheik310@gmail.com>
2024-11-16 12:29:30 +0530

    Update GetText.java

--- Selenium/src/webelements/GetText.java
+++ Selenium/src/webelements/GetText.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GetText {
 	public static void main(String[] args) throws InterruptedException {
+		//Modification done in GitHub
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://demowebshop.tricentis.com/login");
